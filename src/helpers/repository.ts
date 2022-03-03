459b0e51aa2c8ef2dba49872f11f97bdbb2521cc
Ashutosh Hathidara <ashutoshhathidara98@gmail.com>
2022-03-03 18:28:21 -0500

    Check error

--- src/helpers/repository.ts
+++ src/helpers/repository.ts
@@ -138,6 +138,7 @@ export async function pushTemplateBlobContent(
         reportBranchConfig: any): Promise<any> {
     // let content: string = await apiOps.getTemplateFileText(); // getReportTemplateContent();
     // console.log(content);
+    // test comment
     console.log(owner, repository);
     let contentTree = await createFileTreeFromTemplate(
         octokit,
